206fb9023d089f0432483a93ff9cf9a7b75cd5d9
Joonsoo Jeon <giyeok7@gmail.com>
2023-08-02 17:30:13 +0900

    Version up

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
   id("java")
-  id("org.jetbrains.kotlin.jvm") version "1.8.0"
-  id("org.jetbrains.intellij") version "1.13.3"
+  id("org.jetbrains.kotlin.jvm") version "1.9.0"
+  id("org.jetbrains.intellij") version "1.15.0"
 }
 
 group = "com.giyeok.bibix"
@@ -37,10 +37,10 @@ dependencies {
 // Configure Gradle IntelliJ Plugin
 // Read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
 intellij {
-  version.set("2023.1")
+  version.set("2023.2")
   type.set("IC") // Target IDE Platform
 
-  plugins.set(listOf("com.intellij.java", "org.intellij.scala:2023.1.16"))
+  plugins.set(listOf("com.intellij.java", "org.intellij.scala:2023.2.17"))
 }
 
 tasks {
@@ -59,8 +59,8 @@ tasks {
 
   patchPluginXml {
     version.set("0.0.8")
-    sinceBuild.set("231")
-    untilBuild.set("231.*")
+    sinceBuild.set("232")
+    untilBuild.set("232.*")
   }
 
   signPlugin {
